b81644a1ae4e111ddc37697ed9333e4edeecd4a0
Hussainjr <syedhussainyjr@gmail.com>
2025-07-03 22:36:56 +0530

    Initial commit

---
+++ adminportal/src/main/java/com/E_BookShop_Springboot/E_BookShop_Springboot/EBookShopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.E_BookShop_Springboot.E_BookShop_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookShopSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookShopSpringbootApplication.class, args);
+	}
+
+}
---
+++ adminportal/src/test/java/com/E_BookShop_Springboot/E_BookShop_Springboot/EBookShopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_BookShop_Springboot.E_BookShop_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookShopSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
